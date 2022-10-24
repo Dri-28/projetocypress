f4845b1bfdba5f939811ab9c6e28fbec5f4afeea
Adriana Correia <adrianacorreia1991@gmail.com>
2022-10-24 19:16:08 -0300

    Viewport e Base Url

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+     
     },
+    viewportWidth:1440,
+    viewportHeight:900,
+    baseUrl:"https://buger-eats.vercel.app"
   },
 });
--- cypress/pages/SignupPage.js
+++ cypress/pages/SignupPage.js
@@ -1,7 +1,6 @@
 class SignupPage{
     go(){
-        cy.viewport(1440,900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
 
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
