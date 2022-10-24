cee6be10a97d53a667c86ad728d8a327267010d3
Adriana Correia <adrianacorreia1991@gmail.com>
2022-10-24 18:49:42 -0300

    Padrão de projeto com Page Object

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,11 +1,8 @@
+import SignupPage from '../pages/SignupPage'
+
 describe('Cadastro',()=>{
     it('Usuário deve s tornar um entregador',()=>{
-        cy.viewport(1440,900)
-        cy.visit('https://buger-eats.vercel.app')
-
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
-
+     
         var deliver={
             name:'Fernando Papito',
             cpf:'00000014141',
@@ -23,43 +20,17 @@ describe('Cadastro',()=>{
             cnh:'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name)
-        cy.get('input[name="cpf"]').type(deliver.cpf)
-        cy.get('input[name="email"]').type(deliver.email)
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+        var signup= new SignupPage();
 
-        cy.get('input[name="postalcode"').type(deliver.adress.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
-
-        cy.get('input[name="address-number"]').type(deliver.adress.number)
-        cy.get('input[name="address-details"]').type(deliver.adress.details)
-
-        cy.get('input[name="address"]').should('have.value',deliver.adress.street)
-        cy.get('input[name="district"]').should('have.value',deliver.adress.district)
-        cy.get('input[name="city-uf"]').should('have.value',deliver.adress.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image"]').attachFile('/images/'+deliver.cnh)
-
-        cy.get('form button[type="submit"]').click()
-
-        const expectedMessage='Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato'
-
-        cy.get('.swal2-container .swal2-html-container')
-           .should('have.text',expectedMessage)
-        
-
+        signup.go();
+        signup.fillForm(deliver);
+        signup.submit();
+        const expectedMessage='Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
 
     })
 
     it('CPF',()=>{
-        cy.viewport(1440,900)
-        cy.visit('https://buger-eats.vercel.app')
-
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
-
         var deliver={
             name:'Fernando Papito',
             cpf:'00000014141AA',
@@ -76,32 +47,12 @@ describe('Cadastro',()=>{
             delivery_method:'Moto',
             cnh:'cnh-digital.jpg'
         }
- 
-        cy.get('input[name="name"]').type(deliver.name)
-        cy.get('input[name="cpf"]').type(deliver.cpf)
-        cy.get('input[name="email"]').type(deliver.email)
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
 
-        cy.get('input[name="postalcode"').type(deliver.adress.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
+        var signup= new SignupPage();
 
-        cy.get('input[name="address-number"]').type(deliver.adress.number)
-        cy.get('input[name="address-details"]').type(deliver.adress.details)
-
-        cy.get('input[name="address"]').should('have.value',deliver.adress.street)
-        cy.get('input[name="district"]').should('have.value',deliver.adress.district)
-        cy.get('input[name="city-uf"]').should('have.value',deliver.adress.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-
-
-        cy.get('input[accept^="image"]').attachFile('/images/'+deliver.cnh)
-
-        cy.get('form button[type="submit"]').click()
-
-         cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
-
-
+        signup.go();
+        signup.fillForm(deliver);
+        signup.submit();
+        signup.alertMessageShouldBe('Oops! CPF inválido')
     })
 })
---
+++ cypress/pages/SignupPage.js
@@ -0,0 +1,47 @@
+class SignupPage{
+    go(){
+        cy.viewport(1440,900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
+
+    }
+
+    fillForm(deliver){
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"').type(deliver.adress.postalcode)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(deliver.adress.number)
+        cy.get('input[name="address-details"]').type(deliver.adress.details)
+
+        cy.get('input[name="address"]').should('have.value',deliver.adress.street)
+        cy.get('input[name="district"]').should('have.value',deliver.adress.district)
+        cy.get('input[name="city-uf"]').should('have.value',deliver.adress.city_state)
+
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
+
+        cy.get('input[accept^="image"]').attachFile('/images/'+deliver.cnh)
+    }
+
+    submit(){
+        cy.get('form button[type="submit"]').click()
+    }
+
+    modalContentShouldBe(expectedMessage){
+        cy.get('.swal2-container .swal2-html-container')
+        .should('have.text',expectedMessage)
+    }
+
+    alertMessageShouldBe(expectedMessage){
+        cy.get('.alert-error').should('have.text',expectedMessage )
+
+    }
+}
+
+export default SignupPage;
